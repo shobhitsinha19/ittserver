89866d075ac01bf7609a9441a1cd391d6c544402
divyanshdeora <divyansh.deora55@gmail.com>
2016-11-19 00:08:41 +0530

    added dash controller

--- Server/public/js/controllers/MainController.js
+++ Server/public/js/controllers/MainController.js
@@ -20,3 +20,52 @@ app.controller("loginCtrl",['$scope','$http',function($scope, $http){
 		$http.post('http://localhost:3000/login',data);
 	}
 }])
+app.directive('file', function () {
+    return {
+        scope: {
+            file: '='
+        },
+        link: function (scope, el, attrs) {
+            el.bind('change', function (event) {
+                var file = event.target.files[0];
+                scope.file = file ? file : undefined;
+                scope.$apply();
+            });
+        }
+    };
+});
+app.controller("dashCtrl",["$scope","$http",function($scope,$http){
+	$scope.upload=function(){	
+
+	 $http({
+            method: 'POST',
+            url: '/upload-file',
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            },
+            data: {
+                email: Utils.getUserInfo().email,
+                token: Utils.getUserInfo().token,
+                upload: $scope.file
+            },
+            transformRequest: function (data, headersGetter) {
+                var formData = new FormData();
+                angular.forEach(data, function (value, key) {
+                    formData.append(key, value);
+                });
+
+                var headers = headersGetter();
+                delete headers['Content-Type'];
+
+                return formData;
+            }
+        })
+        .success(function (data) {
+        	
+        })
+        .error(function (data, status) {
+
+        });	    
+
+	}	
+}])
